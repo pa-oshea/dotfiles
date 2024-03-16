220bb7fdb87526ad0f12932e78630ef0dfa33018
Patrick O'Shea <p14oshea@gmail.com>
2024-03-16 12:51:06 +0000

    Wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("JetBrains Mono"),
+	color_scheme = "Catppuccin Mocha",
+	window_padding = {
+		left = 0,
+		right = 0,
+		bottom = 0,
+	},
+}
