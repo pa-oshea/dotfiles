133893dd7343ad62ebc13dbc71c8f59268e1b67b
Patrick O'Shea <p14oshea@gmail.com>
2024-05-26 19:24:10 +0100

    Update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -15,5 +15,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Check monitors https://github.com/polybar/polybar/issues/763
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+	MONITOR=$m polybar -q main -c "$DIR"/config.ini &
 done
