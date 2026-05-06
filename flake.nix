c526e26739aea1e229ec77f77612fb1663e7a070
Patrick O'Shea <p14oshea@gmail.com>
2026-05-06 18:35:13 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,34 +1,33 @@
 {
   description = "Dev Environment Flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      mkEnv = name: paths: pkgs.buildEnv { inherit name paths; };
     in {
       packages.${system} = {
-        # Combine everything for a 'full' install
-        default = pkgs.buildEnv {
-          name = "my-complete-env";
-          paths = (import ./nix/core.nix { inherit pkgs; })
-               ++ (import ./nix/java.nix { inherit pkgs; });
-        };
+        default = mkEnv "my-complete-env" (
+          import ./nix/core.nix  { inherit pkgs; } ++
+          import ./nix/java.nix  { inherit pkgs; } ++
+          import ./nix/go.nix    { inherit pkgs; } ++
+          import ./nix/rust.nix  { inherit pkgs; } ++
+          import ./nix/node.nix  { inherit pkgs; }
+        );
+        core = mkEnv "core-env"  (import ./nix/core.nix  { inherit pkgs; });
+        java = mkEnv "java-env"  (import ./nix/java.nix  { inherit pkgs; });
+        go   = mkEnv "go-env"    (import ./nix/go.nix    { inherit pkgs; });
+        rust = mkEnv "rust-env"  (import ./nix/rust.nix  { inherit pkgs; });
+        node = mkEnv "node-env"  (import ./nix/node.nix  { inherit pkgs; });
+      };
 
-        # Just the core tools
-        core = pkgs.buildEnv {
-          name = "core-env";
-          paths = import ./nix/core.nix { inherit pkgs; };
-        };
-
-        # Just the Java tools
-        java = pkgs.buildEnv {
-          name = "java-env";
-          paths = import ./nix/java.nix { inherit pkgs; };
-        };
+      # Project-level shells (nix develop .#rust-project etc.)
+      devShells.${system} = {
+        rust-project = import ./shells/rust.nix { inherit pkgs; };
+        java-project = import ./shells/java.nix { inherit pkgs; };
       };
     };
 }
